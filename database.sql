810f4f3f24beff3c7f48a6aeea0015c708b837ce
phitranpt <phi.tran.pt@gmail.com>
2018-10-16 11:04:37 -0500

    added questions to answer

---
+++ database.sql
@@ -0,0 +1,9 @@
+-- 0. Get all users from Chicago.
+-- 1. Get all users with usernames that contain the letter a.
+-- 2. The bank is giving a new customer bonus! Update all records with an account balance of 0.00 and a transactions_attempted of 0. Give them a new account balance of 10.00.
+-- 3. Select all users that have attempted 9 or more transactions.
+-- 4. Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
+-- 5. Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
+-- 6. Get all users with account balances that are more than $100.
+-- 7. Add a new record.
+-- 8. The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: Delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
